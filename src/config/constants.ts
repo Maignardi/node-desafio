b6e22731a0e6c7786b7df62c83efe1198f316659
Maignardi <mmaignardi@hotmail.com>
2021-12-07 11:59:14 -0400

    confirma

---
+++ src/config/constants.ts
@@ -0,0 +1,6 @@
+
+export const config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION: 
+    'mongodb+srv://mmaignardi@hotmail.com.go52y.mongodb.net/urlshortener?retryWrites=true&w=majority'
+}
